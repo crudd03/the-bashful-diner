e76df106d373edbc3594ff064e2546bb071a5d61
Corey Rapala <coreyrapala@gmail.com>
2021-05-06 14:35:57 -0400

    started server request button functionality

--- models/GuestTable.js
+++ models/GuestTable.js
@@ -1,5 +1,5 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 class GuestTable extends Model {}
 
@@ -9,18 +9,22 @@ GuestTable.init(
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true
+      autoIncrement: true,
     },
     table_number: {
       type: DataTypes.INTEGER,
       allowNull: false,
+    },
+    serverRequested: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
   },
   {
     sequelize,
     freezeTableName: true,
     underscored: true,
-    modelName: 'guesttable'
+    modelName: "guesttable",
   }
 );
 
--- utils/auth.js
+++ utils/auth.js
@@ -1,7 +1,7 @@
 const withAuth = (req, res, next) => {
   // If the user is not logged in, redirect the request to the login route
-  if (!req.session.logged_in) {
-    res.redirect('/login');
+  if (!req.session.logged_in && !req.session.user_id) {
+    res.redirect("/login");
   } else {
     next();
   }
