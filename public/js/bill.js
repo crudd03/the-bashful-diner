1b254eaf85f0a4241b04dab9b640093273c0e244
KJ <kj3641402@gmail.com>
2021-05-07 02:35:16 -0400

    added modal for cash out

--- public/js/bill.js
+++ public/js/bill.js
@@ -1,29 +1,53 @@
-// sums for bill
-// call server button
-const requestServerHandler = async (event) => {
+const requestCashOut = async (event) => {
   event.preventDefault();
-
-  const response = await fetch("/api/table/request", {
-    method: "PUT",
-    headers: { "Content-Type": "application/json" },
-  });
-
-  if (!response.ok) {
-    alert(JSON.stringify(response));
-  }
-
-  const logoutResponse = await fetch("/api/user/logout", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-  });
-
-  if (logoutResponse.ok) {
-    document.location.replace("/");
-  } else {
-    alert(response.statusText);
-  }
-};
-
+  swal.fire({
+    title: 'Would you like to conclude your dining experience?',
+    icon: 'question',
+    showCancelButton: true,
+    confirmButtonColor: '#46c496',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Please bring my Tab!',
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire({
+        icon: 'success',
+        iconColor: '#46c496',
+        title: 'Thank You!',
+        text: 'Your server will be with you shortly!',
+        showConfirmButton: true,
+       }).then( async ()  => {
+     
+        const response = await fetch("/api/table/request", {
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
+        });
+      
+        if (!response.ok) {
+          alert(JSON.stringify(response));
+        }
+      
+        const logoutResponse = await fetch("/api/user/logout", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+        });
+      
+        if (logoutResponse.ok) {
+          document.location.replace("/");
+        } else {
+          alert(response.statusText);
+        }
+        });
+    } else {
+        Swal.fire({
+          icon: 'error',
+          title: 'No Worries!',
+          text: 'Enjoy the Rest of your visit!',
+          showConfirmButton: false,
+          timer: 2000
+         })
+        }
+  })
+}  
 document
   .querySelector("#requestServer")
-  .addEventListener("click", requestServerHandler);
+  .addEventListener("click", requestCashOut);
